013882c6d3f39f75908a1dd100ff9330040545ae
mrtanyudi <yudi98@live.com>
2017-08-04 10:20:04 -0700

    validate post contents when editing

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,3 +1,17 @@
+Template.postEdit.onCreated(function(){
+  Session.set('postEditErrors', {})
+});
+
+Template.postEdit.helpers({
+  errorMessage: function(field){
+    return Session.get('postEditErrors')[field]
+  },
+  errorClass: function(field){
+    return !!Session.get('postEditErrors')[field] ? 'has-error' : ''
+  }
+});
+
+
 Template.postEdit.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -5,6 +19,12 @@ Template.postEdit.events({
     var postProperties = {
       url: $(e.target).find('[name=url]').val(), title: $(e.target).find('[name=title]').val()
     }
+
+    var errors = validatePost(postProperties);
+    if (errors.title || errors.url) {
+      return Session.set('postEditErrors', errors)
+    };
+
     Posts.update(currentPostId, {$set: postProperties}, function(error) {
       if (error) {
         // display the error to the user
